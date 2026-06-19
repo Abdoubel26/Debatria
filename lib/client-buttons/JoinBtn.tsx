bcde5075c0ffd52d95ab184ba4f27a15e1037344
Abdoubel26 <abdoubelounis09@gmail.com>
2026-06-19 15:23:12 +0100

    edit join button's color

--- lib/client-buttons/JoinBtn.tsx
+++ lib/client-buttons/JoinBtn.tsx
@@ -12,16 +12,18 @@ function JoinBtn({topic}: PropTypes) {
     const { pending } = useFormStatus()
 
   return (
-     <button
-    disabled={pending}
-    formAction={ topic.secondParticipant ? () => redirect(`/chat/${topic.id}`) : joinDebate.bind(null, topic.id)}
-                className={`rounded-xl cursor-pointer px-4 py-2 text-sm font-medium transition-all ${
-                  topic.status === "open"
-                    ? ( pending ? "bg-slate-200 text-slate-800" : "bg-slate-100 text-slate-950 hover:bg-white shadow-sm")
-                    : "dark:bg-slate-800 bg-slate-300 hover:bg-slate-400 text-black dark:text-slate-300 dark:hover:bg-slate-700 border border-slate-700"
-                }`}
-              >
-               {topic.status === "open" ?( pending ? "Joining..." : "Join Debate") : "View Debate"}
+    <button
+      disabled={pending}
+      formAction={ topic.secondParticipant ? () => redirect(`/chat/${topic.id}`) : joinDebate.bind(null, topic.id)}
+      className={`rounded-xl cursor-pointer px-4 py-2 text-sm font-medium transition-all ${
+        topic.status === "open"
+          ? ( pending 
+              ? "bg-slate-300 dark:bg-slate-700 text-slate-800 dark:text-slate-300 cursor-not-allowed" 
+              : "bg-slate-100 dark:bg-slate-800 text-slate-950 dark:text-white hover:bg-white dark:hover:bg-slate-700 shadow-sm")
+          : "bg-slate-300 dark:bg-slate-800 text-slate-900 dark:text-slate-300 hover:bg-slate-400 dark:hover:bg-slate-700 border border-slate-400 dark:border-slate-700"
+      }`}
+    >
+      {topic.status === "open" ? ( pending ? "Joining..." : "Join Debate") : "View Debate"}
     </button>
   )
 }
