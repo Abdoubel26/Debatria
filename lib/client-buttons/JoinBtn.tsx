4a934db94e2c0087eb7dc16ba3e4cdd2af992d1b
Abdoubel26 <abdoubelounis09@gmail.com>
2026-06-19 15:19:02 +0100

    edit one color

--- lib/client-buttons/JoinBtn.tsx
+++ lib/client-buttons/JoinBtn.tsx
@@ -18,7 +18,7 @@ function JoinBtn({topic}: PropTypes) {
                 className={`rounded-xl cursor-pointer px-4 py-2 text-sm font-medium transition-all ${
                   topic.status === "open"
                     ? ( pending ? "bg-slate-200 text-slate-800" : "bg-slate-100 text-slate-950 hover:bg-white shadow-sm")
-                    : "dark:bg-slate-800 bg-slate-300 hover:bg-slate-400 text-slate-300 dark:hover:bg-slate-700 border border-slate-700"
+                    : "dark:bg-slate-800 bg-slate-300 hover:bg-slate-400 text-black dark:text-slate-300 dark:hover:bg-slate-700 border border-slate-700"
                 }`}
               >
                {topic.status === "open" ?( pending ? "Joining..." : "Join Debate") : "View Debate"}
